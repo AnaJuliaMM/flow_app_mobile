48a1bd9fade4a88c74b4ad0da6e903e73fedc094
camiyuka <camila.yatabe@gmail.com>
2023-11-28 20:48:38 -0300

    feat(SensorOutput): init component

---
+++ components/SensorOutput.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SensorOutput() {
+  return (
+    <View>
+      <Text>SensorOutput</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
