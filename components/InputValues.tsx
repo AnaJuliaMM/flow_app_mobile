5c9978b38d90f4ab92fb46d5354f1307327d6f3e
AnaJuliaMM <ana.martins_2004@outlook.com>
2023-11-30 20:31:09 -0300

    chore(InputValues): styly and add Image components

--- components/InputValues.tsx
+++ components/InputValues.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, TextInput  } from 'react-native'
 import React from 'react'
+import CustomImage from './Image'
 
 interface InputValuesProp{
     text: string,
@@ -7,31 +8,50 @@ interface InputValuesProp{
 }
 
 export default function InputValues({text, imagePath}: InputValuesProp) {
+  
+
   return (
-    <View >
-        <Image source={{uri: imagePath}}/>
+    <View style={styles.wrapper}>
+      <Text style={styles.text}>{text}</Text>
       <View style={styles.container}>
-        <Text style={styles.text}>{text}</Text>
-        <input style={styles.input} type="number" />
+        <CustomImage imagePath='../assets/images/vector_oil.png' style={{width: 27, height: 30}}/>
+        <TextInput  keyboardType='numeric' style={styles.input}/>
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 10,
-        width: '100%'
-    },
-    text: {
-        color: '#fff'
-    },
-    input: {
-        border: 'none', 
-        width: 100
-
-    }
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+  container: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 20,
+      width: '100%'
+  },
+  text: {
+      color: '#000',
+      fontSize: 30
+  },
+  input: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    border: 'none',
+    width: 100,
+    height: 70,
+    borderRadius: 0.5,
+    fontSize: 30,
+    fontWeight: '700',
+  },
+  image: {
+    width: 17,
+    height: 30
+  }
 })
